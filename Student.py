afc6c7ec40deb08d7b2138233a7dc2c562fba4b8
Sudeep Shivnikar <31180001+sdshivnikar@users.noreply.github.com>
2017-11-22 23:01:47 +0530

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,6 +1,5 @@
 '''
 Author : Shivnikar Sudeep 
-Date : October 30 , 2017 
 Problem : Predicting whether a student will pass or fail depending on his/her habits & previous record
 Dataset Credits : https://archive.ics.uci.edu/ml/datasets/Student+Performance
 
